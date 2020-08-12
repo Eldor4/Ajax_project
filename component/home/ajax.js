d30c630e9bd7b78a11a85b258eb252708bccab7d
Eldor4 <65150710+Eldor4@users.noreply.github.com>
2020-08-12 18:31:32 +0300

    v2.2

--- component/home/ajax.js
+++
@@ -1,8 +0,0 @@
-// פשוטה   AJAX פונקציית
-//   function ajaxFun(url,cb){
-//     let xhr = new XMLHttpRequest();
-//     xhr.open('GET', url);
-//     xhr.responseType = 'json';
-//     xhr.send();
-//     xhr.addEventListener('load', cb);}
-//     export default ajaxFun
